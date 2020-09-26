ea7d962c5194b14f7bf0587bac43de249e07e1cb
jingarn <jingarn+github@gmail.com>
2020-09-26 19:12:37 +0900

    clean files

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,6 @@
 class ProfilesController < ApplicationController
   def new
+    @parents = Category.where(ancestry: nil)
   end
   def create
     redirect_to action: :new
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    get "/sign_up" => "users/registrations#new", as: "new_user_registration"
+    get "/sign_in" => "users/sessions#new", as: "user_session"
+  end
   devise_for :users
+
   root 'items#index'
   resources :users, only: [:index, :edit] 
   resources :cards, only: [:new, :show, :destroy] 
