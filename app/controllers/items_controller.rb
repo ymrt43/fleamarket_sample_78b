c69390c68251204b138d10698014cb4e23186233
ymrt43 <ymrt43@gmail.com>
2020-10-04 01:55:27 +0900

    Add items#create definition

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,6 +8,15 @@ class ItemsController < ApplicationController
     @item.images.new
   end
   
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  
   def show
     @parents = Category.where(ancestry: nil)
   end
