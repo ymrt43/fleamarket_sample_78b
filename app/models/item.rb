6304a294e4e5dbe29ec70da85afb2b8a417516ea
ymrt43 <ymrt43@gmail.com>
2020-10-04 01:41:11 +0900

    Item nest with Image model

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,7 @@ class Item < ApplicationRecord
   belongs_to :user
   belongs_to :category
   belongs_to :buyer, class_name: "User"
+  accepts_nested_attributes_for :images, allow_destroy: true
 
   validates :name, :description, :state, :fee, :area, :term, :price, :user_id, category_id, presence: true
 end
