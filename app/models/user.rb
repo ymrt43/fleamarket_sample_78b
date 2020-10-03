6cb059fd365c915bef760fdfb3379d6a31f93223
ymrt43 <ymrt43@gmail.com>
2020-10-04 01:06:45 +0900

    Add associations with model User about Item
    definition about type of items
    on 3 way of conditional branch
    1_ buyed_items
    2_ sell_items
    3_ sold_items

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one :profile
+  has_many :buyed_items, foreign_key: "buyer_id", class_name: "Item"
+  has_many :sell_items, -> { where("buyer_id is NULL") }, foreign_key: "user_id", class_name: "Item"
+  has_many :sold_items, -> { where("buyer_id is not NULL") }, foreign_key: "user_id", class_name: "Item"
 
   validates :name, presence: true, uniqueness: true
 end
