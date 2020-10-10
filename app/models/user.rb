1a44d207414e318a07b5360694a64691b6796063
ymrt43 <ymrt43@gmail.com>
2020-10-10 12:44:20 +0900

    delete validation of :name's uniqueness in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,12 @@ class User < ApplicationRecord
   has_many :buyed_items, foreign_key: "buyer_id", class_name: "Item"
   has_many :sell_items, -> { where("buyer_id is NULL") }, foreign_key: "user_id", class_name: "Item"
   has_many :sold_items, -> { where("buyer_id is not NULL") }, foreign_key: "user_id", class_name: "Item"
-  
+
   has_one :profile, dependent: :destroy
   accepts_nested_attributes_for :profile
 
   has_one :address, dependent: :destroy
   accepts_nested_attributes_for :address
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
 end
