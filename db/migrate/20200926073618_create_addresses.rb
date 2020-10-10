211e453e382718523c320914eb068fa182680dbf
jingarn <jingarn+github@gmail.com>
2020-10-10 10:07:03 +0900

    update address

--- db/migrate/20200926073618_create_addresses.rb
+++ db/migrate/20200926073618_create_addresses.rb
@@ -2,7 +2,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
       t.integer :postalcode, null: false
-      t.integer :prefecture, null: false
+      t.string :prefecture, null: false
       t.string :city, null: false
       t.string :house_number, null: false
       t.string :building_number, null: false
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2020_09_26_073618) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "postalcode", null: false
-    t.integer "prefecture", null: false
+    t.string "prefecture", null: false
     t.string "city", null: false
     t.string "house_number", null: false
     t.string "building_number", null: false
