0bf28d77b536828d7041709b162628d8a06452bb
jingarn <jingarn+github@gmail.com>
2020-10-11 03:20:53 +0900

    Update address.rb

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,8 @@
 class Address < ApplicationRecord
   belongs_to :user, optional: true
 
-  validates :postalcode, :prefecture, :city, :house_number, presence: true
+  VALID_PCODE_REGEX = /\A\d{7}\z/
+
+  validates :postalcode, presence: true, format: { with: VALID_PCODE_REGEX }
+  validates :prefecture, :city, :house_number, presence: true
 end
