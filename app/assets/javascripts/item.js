1073853ac2e9f80e8fc11ea0acb31eeea4ab359f
ymrt43 <ymrt43@gmail.com>
2020-10-11 02:08:49 +0900

    Add to call buildFileField with fileIndex

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -2,4 +2,9 @@ $(function(){
 
   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
 
+  $('.FormBlock__iSelectsField').on('change', '.FormBlock__iSelection--file', function(e) {
+    $('.FormBlock__iSelectsField').append(buildFileField(fileIndex[0]));
+    fileIndex.shift();
+    fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
+  });
 });
