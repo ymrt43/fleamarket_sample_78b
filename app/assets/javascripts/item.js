6c9ecc8fec4db47bb2688d9dc1e8269a9f00fa99
ymrt43 <ymrt43@gmail.com>
2020-10-11 02:58:21 +0900

    Add function buildFileField in item.js

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -1,4 +1,17 @@
 $(function(){
+  function buildFileField(index) {
+    const html = `<div data-index="${index} "class="FormBlock__iSelection">
+                    <input required="required" class="FormBlock__iSelection--file" type="file" name="item[images_attributes][${index}][src]" id="item_images_attributes_${index}_src">
+                    <i class="fas fa-camera FormBlock__iSelection--icon"></i>
+                    <p class="FormBlock__iSelection--hwToUp">
+                    ドラッグアンドドロップ
+                    <br>
+                    またはクリックしてファイルをアップロード
+                    </p>
+                    <span class="FormBlock__iSelection--remove">削除</span>
+                  </div>`;
+    return html;
+  }
 
   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
 
@@ -8,7 +21,7 @@ $(function(){
     fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
   });
 
-  $('.FormBlock__iSelectsField').on('click', '.FormBlock__iSelection--remove', funciton(e) {
+  $('.FormBlock__iSelectsField').on('click', '.FormBlock__iSelection--remove', function(e) {
     $(this).parent().remove();
     if ($('.FormBlock__iSelectsField').length == 0) $('.FormBlock__iSelectsField').append(buildFileField(fileIndex[0]));
   });
