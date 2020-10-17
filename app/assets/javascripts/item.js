4215ba8fc15616a2c9dc52e0ce110cee7847eb38
ymrt43 <ymrt43@gmail.com>
2020-10-17 12:13:13 +0900

    Update motions in item.js to select file of images

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -1,29 +1,60 @@
 $(function(){
-  function buildFileField(index) {
-    const html = `<div data-index="${index} "class="FormBlock__iSelection">
-                    <input required="required" class="FormBlock__iSelection--file" type="file" name="item[images_attributes][${index}][src]" id="item_images_attributes_${index}_src">
-                    <i class="fas fa-camera FormBlock__iSelection--icon"></i>
-                    <p class="FormBlock__iSelection--hwToUp">
-                    ドラッグアンドドロップ
-                    <br>
-                    またはクリックしてファイルをアップロード
-                    </p>
-                    <span class="FormBlock__iSelection--remove">削除</span>
+  function buildFileField(num) {
+    const html = `<div data-index="${num}" class="ImageBox__fileGroup">
+                    <input class="ImageBox__file" type="file"
+                    name="item[images_attributes][${num}][src]"
+                    id="item_images_attributes_${num}_src"><br>
+                  </div>`;
+    return html;
+  }
+
+  function buildImg(index, url) {
+    const html = `<div class="ImageBox__preview">
+                    <img data-index="${index}" src="${url}" width="100px" height="100px">
+                    <div class="ImageBox__remove">削除</div>
                   </div>`;
     return html;
   }
 
   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
+  lastIndex = $('.ImageBox__fileGroup:last').data('index');
+  fileIndex.splice(0, lastIndex);
 
-  $('.FormBlock__iSelectsField').on('change', '.FormBlock__iSelection--file', function(e) {
-    e.preventDefault()
-    $('.FormBlock__iSelectsField').append(buildFileField(fileIndex[0]));
-    fileIndex.shift();
-    fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
+  $('.hidden-destroy').hide();
+
+  $('#ImageBox').on('change', '.ImageBox__file', function(e) {
+    const targetIndex = $(this).parent().data('index');
+    const file = e.target.files[0];
+    const blobUrl = window.URL.createObjectURL(file);
+
+    if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
+      img.setAttribute('src', blobUrl);
+    } else {
+      $('#previews').prepend(buildImg(targetIndex, blobUrl));
+      $('.ImageBox__hidden').append(buildFileField(fileIndex[0]));
+      $('.ImageBox__inputLabel').attr({id: `label-box-${fileIndex[0]}`,for: `item_images_attributes_${fileIndex[0]}_src`});
+      fileIndex.shift();
+      fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
+    }
+    var count = $('.ImageBox__preview').length;
+    if (count >= 10) {
+      $('.ImageBox__inputLabel').hide();
+    }
   });
 
-  $('.FormBlock__iSelectsField').on('click', '.FormBlock__iSelection--remove', function() {
+  $('#ImageBox').on('click', '.ImageBox__remove', function() {
+    const targetIndex = $(this).parent().data('index');
+    console.log(targetIndex)
+    const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
+    if (hiddenCheck) hiddenCheck.prop('checked', true);
+
     $(this).parent().remove();
-    if ($('.FormBlock__iSelectsField').length == 0) $('.FormBlock__iSelectsField').append(buildFileField(fileIndex[0]));
+    $(`img[data-index="${targetIndex}"]`).remove();
+    var count = $('.ImageBox__preview').length;
+    if (count <= 9) {
+      $('.ImageBox__inputLabel').show();
+    }
+
+    if ($('.ImageBox__file').length == 0) $('#ImageBox').append(buildFileField(fileIndex[0]));
   });
 });
