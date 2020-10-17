1fd8202345f3be9d05577b4e62b3cf289372667c
ymrt43 <ymrt43@gmail.com>
2020-10-17 21:08:16 +0900

    Recreate DB

--- db/migrate/20200930101520_create_items.rb
+++ db/migrate/20200930101520_create_items.rb
@@ -2,14 +2,15 @@ class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
       t.string :name, limit: 40, null: false, index: true
-      t.text :description, null: false
+      t.text :description, limit: 1000, null: false
       t.string :brand
       t.integer :state, null: false
       t.integer :fee, null: false
+      t.integer :prefecture_id, null: false
       t.integer :term, null: false
       t.integer :price, null: false
-      t.integer :buyer_id
-      t.references :user, index: true, foreign_key: true
+      t.references :seller, index: true, foreign_key: {to_table: :users}
+      t.references :buyer, index: true, foreign_key: {to_table: :users}
       t.references :category, index: true, foreign_key: true
       t.timestamps
     end
