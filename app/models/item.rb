b8087fb0dda0fa00b6e767c89abf13f119215520
ymrt43 <ymrt43@gmail.com>
2020-10-18 21:42:17 +0900

    Add validations of Item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,19 @@
 class Item < ApplicationRecord
-  has_many :images, dependent: :destroy
+  has_many :images, dependent: :destroy, inverse_of: :item
+  accepts_nested_attributes_for :images, allow_destroy: true
   has_many :comments
   belongs_to :seller, class_name: "User", foreign_key: "seller_id"
   belongs_to :category
   belongs_to :buyer, class_name: "User", optional: true, foreign_key: "buyer_id"
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
-  accepts_nested_attributes_for :images, allow_destroy: true
 
-  validates :name, :description, :state, :fee, :term, :price, presence: true
+  validates :images, presence: true, length: { minimum: 1, maximum: 10 }
+  validates :name, presence: true, length: { maximum: 40, too_long: "最大%{count}文字まで使えます" }
+  validates :description, presence: true, length: { maximum: 1000, too_long: "最大%{count}文字まで使えます" }
+  validates :price, presence: true, inclusion: 300..9999999
+  validates :state, :fee, :term, presence: true
 
   enum state: {
     "新品、未使用": 1, "未使用に近い": 2, "目立った傷や汚れなし": 3,
