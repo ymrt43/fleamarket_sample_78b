ddee7d3e85c5b6764fe134d26ccdbf255e195881
akyhysa <m_akihisa8566@yahoo.co.jp>
2020-10-23 00:05:00 +0900

    edit-item-show

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -1,10 +1,7 @@
 FactoryBot.define do
   factory :card do
-    # id {1}
-    # user_id {1}
     customer_id { "cus_xxxxxxxxxxxxxx" }
     card_id { "car_yyyyyyyyyyyyyyy" }
-    user = FactoryBot.create(:user)
-    # association :user
+    # user = FactoryBot.create(:user)
   end
 end
