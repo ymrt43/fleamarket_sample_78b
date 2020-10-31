5b577538fdf581dfc9b0966967fb2b60301452c4
ymrt43 <ymrt43@gmail.com>
2020-10-31 17:53:31 +0900

    Add routing for omniauth_callbacks and Modify routes.rb not to use hash rockets

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :controllers => {
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions'   
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
   } 
 
   devise_scope :user do
-    get "sign_in", :to => "users/sessions#new"
-    get "sign_out", :to => "users/sessions#destroy" 
+    get "sign_in", to: "users/sessions#new"
+    get "sign_out", to: "users/sessions#destroy" 
   end
 
   root 'items#index'
