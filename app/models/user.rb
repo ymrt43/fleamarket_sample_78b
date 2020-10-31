b424420bb5af8596f93550f4a97581050e656898
ymrt43 <ymrt43@gmail.com>
2020-10-31 17:39:29 +0900

    Add option & Set relation between User & SnsCredential

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
+
   has_many :buyed_items, foreign_key: "buyer_id", class_name: "Item"
   has_many :sell_items, -> { where("buyer_id is NULL") }, foreign_key: "seller_id", class_name: "Item"
   has_many :sold_items, -> { where("buyer_id is not NULL") }, foreign_key: "seller_id", class_name: "Item"
+  has_many :sns_credentials
 
   has_one :profile, dependent: :destroy, validate: true
   accepts_nested_attributes_for :profile
